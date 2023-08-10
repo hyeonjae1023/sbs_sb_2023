062fa197b632ee1b37c120a870eb96e750acd838
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-08-10 12:11:35 +0900

    spring boot 첫 커밋

---
+++ src/main/java/com/phj/exam/demo/controller/UserHomeController.java
@@ -0,0 +1,27 @@
+package com.phj.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+public class UserHomeController {
+	@RequestMapping("/user/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+	
+	@RequestMapping("/user/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+	
+	@RequestMapping("/user/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요.";
+	}
+	
+}
