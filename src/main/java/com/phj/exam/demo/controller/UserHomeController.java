594b802d17282206093d643a48118a4f97846991
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-08-10 12:29:14 +0900

    새로고침 시 1씩 증가하는 액션 구현

--- src/main/java/com/phj/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/phj/exam/demo/controller/UserHomeController.java
@@ -6,26 +6,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller 
 public class UserHomeController {
+	int count = 0;
 	@RequestMapping("/user/home/main")
 	@ResponseBody
-	public int showMain() {
-		return 0;
+	public String showMain() {
+		return "안녕하세요.";
 	}
-//	@RequestMapping("/user/home/main1")
-//	@ResponseBody
-//	public int showMain2() {
-//		return 1;
-//	}
-//	@RequestMapping("/user/home/main2")
-//	@ResponseBody
-//	public int showMain3() {
-//		return 2;
-//	}
-//	@RequestMapping("/user/home/main3")
-//	@ResponseBody
-//	public int showMain4() {
-//		return 3;
-//	}
-//	
+	@RequestMapping("/user/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+	@RequestMapping("/user/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요";
+	}
+	@RequestMapping("/user/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
+	}
+	
 	
 }
