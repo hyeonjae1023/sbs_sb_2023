94fc6a8d92002cbdad1efb57f3dfa5595c5faa82
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-08-11 13:28:21 +0900

    mapper 연동 후 insert delete modify detail 쿼리문 연결

--- src/main/java/com/phj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/phj/exam/demo/repository/ArticleRepository.java
@@ -2,27 +2,33 @@ package com.phj.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.phj.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(@Param("title") String title,@Param("body") String body);
 
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	
+	@Select("SELECT * FROM article")
 	public List<Article> getArticles();
 
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void removeArticle(@Param("id") int id);
 
-	public void removeArticle(int id);
+	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
 
-
-	public void modifyArticle(int id, String title, String body);
+	@Select("SELECT LAST_INSERT_ID();")
+	public int getLastArticleId();
 
 }
